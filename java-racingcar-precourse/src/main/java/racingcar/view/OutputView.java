acddf90f45ae0274a19fe406d1d71db5d322b370
woo-chang <clllickme@naver.com>
2022-12-02 14:03:10 +0900

    feat(OutputView): 최종 우승자를 출력하는 기능 구현

--- java-racingcar-precourse/src/main/java/racingcar/view/OutputView.java
+++ java-racingcar-precourse/src/main/java/racingcar/view/OutputView.java
@@ -1,11 +1,21 @@
 package racingcar.view;
 
 import java.util.List;
+import java.util.StringJoiner;
+import racingcar.model.Car;
 
 public class OutputView {
 
-    public void printExecuteResult(List<String> cars) {
+    private static final String WINNER = "최종 우승자 : %s";
+
+    public void printExecuteResult(List<Car> cars) {
         cars.forEach(System.out::println);
+    }
+
+    public void printWinner(List<Car> winners) {
+        StringJoiner joiner = new StringJoiner(", ");
+        winners.forEach(winner -> joiner.add(winner.getName()));
+        System.out.printf((WINNER) + "%n", joiner.toString());
     }
 
 }
