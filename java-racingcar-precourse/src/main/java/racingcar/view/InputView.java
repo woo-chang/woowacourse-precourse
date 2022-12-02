b1df637fab4a209143edff1c4a4a6aff5440542a
woo-chang <clllickme@naver.com>
2022-12-02 16:29:52 +0900

    refactor(InputView): 조금 더 명확한 의미를 가지는 변수명으로 수정

--- java-racingcar-precourse/src/main/java/racingcar/view/InputView.java
+++ java-racingcar-precourse/src/main/java/racingcar/view/InputView.java
@@ -4,16 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String TITLE_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    private static final String TITLE_ATTEMPT = "시도할 회수는 몇회인가요?";
+    private static final String CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ATTEMPT = "시도할 회수는 몇회인가요?";
 
     public String readCarNames() {
-        System.out.println(TITLE_CAR_NAMES);
+        System.out.println(CAR_NAMES);
         return Console.readLine();
     }
 
     public String readAttempt() {
-        System.out.println(TITLE_ATTEMPT);
+        System.out.println(ATTEMPT);
         return Console.readLine();
     }
 
