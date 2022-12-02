b2bdad46814b4877f487c565c1051959a8f7703d
woo-chang <clllickme@naver.com>
2022-12-02 15:45:09 +0900

    refactor(AttemptConverter): 에러 메시지 상수화

--- java-racingcar-precourse/src/main/java/racingcar/util/AttemptConverter.java
+++ java-racingcar-precourse/src/main/java/racingcar/util/AttemptConverter.java
@@ -2,13 +2,16 @@ package racingcar.util;
 
 public class AttemptConverter {
 
-    private AttemptConverter() {}
+    private static final String ERROR_MESSAGE = "시도 횟수는 숫자여야 한다.";
+
+    private AttemptConverter() {
+    }
 
     public static int convert(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("시도 횟수는 숫자여야 한다.", e);
+            throw new IllegalArgumentException(ERROR_MESSAGE, e);
         }
     }
 
