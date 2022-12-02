492b77f88fa4772eedcf76281f7e8fdd13ec5eca
woo-chang <clllickme@naver.com>
2022-12-02 15:41:45 +0900

    refactor(InputView): 입력에 사용되는 문구 상수화

--- java-racingcar-precourse/src/main/java/racingcar/view/InputView.java
+++ java-racingcar-precourse/src/main/java/racingcar/view/InputView.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String TITLE_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String TITLE_ATTEMPT = "시도할 회수는 몇회인가요?";
+
     public String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        System.out.println(TITLE_CAR_NAMES);
         return Console.readLine();
     }
 
     public String readAttempt() {
-        System.out.println("시도할 회수는 몇회인가요?");
+        System.out.println(TITLE_ATTEMPT);
         return Console.readLine();
     }
 
