0b0afe5c82a8b1bca1e0f49e0ca28fca994ad223
woo-chang <clllickme@naver.com>
2022-12-02 15:38:47 +0900

    refactor(CarNamesConverter): 분리자 상수화, 변수 및 메서드명 수정

--- java-racingcar-precourse/src/main/java/racingcar/util/CarNamesConverter.java
+++ java-racingcar-precourse/src/main/java/racingcar/util/CarNamesConverter.java
@@ -7,25 +7,26 @@ import java.util.stream.Collectors;
 public class CarNamesConverter {
 
     private static final String ERROR_MESSAGE = "자동차 이름은 공백 제외 1자 이상, 5자 이하만 가능합니다.";
+    private static final String SEPARATOR = ",";
 
     private CarNamesConverter() {
     }
 
     public static List<String> convert(String input) {
-        String[] split = input.split(",");
-        List<String> result = Arrays.stream(split)
+        String[] target = input.split(SEPARATOR);
+        List<String> names = Arrays.stream(target)
                 .map(String::trim)
                 .filter(CarNamesConverter::isValidLength)
                 .collect(Collectors.toList());
-        validateResult(split, result);
-        return result;
+        validateNames(target, names);
+        return names;
     }
 
     private static boolean isValidLength(String name) {
         return name.length() > 0 && name.length() < 6;
     }
 
-    private static void validateResult(String[] input, List<String> result) {
+    private static void validateNames(String[] input, List<String> result) {
         if (input.length != result.size()) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
