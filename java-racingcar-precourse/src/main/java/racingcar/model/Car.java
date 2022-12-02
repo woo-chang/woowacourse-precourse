76d86af9f8cd4fa92fa66505ba487ef2981cfd45
woo-chang <clllickme@naver.com>
2022-12-02 13:42:27 +0900

    feat(Car): 현재 위치가 증가하는 기능 구현

--- java-racingcar-precourse/src/main/java/racingcar/model/Car.java
+++ java-racingcar-precourse/src/main/java/racingcar/model/Car.java
@@ -1,4 +1,4 @@
-package racingcar;
+package racingcar.model;
 
 public class Car {
     private final String name;
@@ -9,4 +9,11 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void move() {
+        position += 1;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
