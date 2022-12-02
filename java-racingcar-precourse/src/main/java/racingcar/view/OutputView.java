c7740143e47ff24fdf629341de69377cadc7eab7
woo-chang <clllickme@naver.com>
2022-12-02 14:29:03 +0900

    feat(OutputView): 에러 메시지를 출력하는 기능 구현

--- java-racingcar-precourse/src/main/java/racingcar/view/OutputView.java
+++ java-racingcar-precourse/src/main/java/racingcar/view/OutputView.java
@@ -7,6 +7,7 @@ import racingcar.model.Car;
 public class OutputView {
 
     private static final String WINNER = "최종 우승자 : %s";
+    private static final String ERROR_MESSAGE = "[ERROR] %s";
 
     public void printExecuteResult(List<Car> cars) {
         cars.forEach(System.out::println);
@@ -16,6 +17,10 @@ public class OutputView {
         StringJoiner joiner = new StringJoiner(", ");
         winners.forEach(winner -> joiner.add(winner.getName()));
         System.out.printf((WINNER) + "%n", joiner.toString());
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.printf((ERROR_MESSAGE) + "%n", message);
     }
 
 }
