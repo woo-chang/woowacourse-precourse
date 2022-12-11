4e34f56226c6e9c9c6af7a5404c43751326bc309
woo-chang <clllickme@naver.com>
2022-12-11 14:43:53 +0900

    feat(CoinBoxGenerator): 랜덤으로 동전 금고를 생성하는 기능 구현

---
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/service/CoinBoxGenerator.java
@@ -0,0 +1,66 @@
+package vendingmachine.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import vendingmachine.model.Coin;
+
+public class CoinBoxGenerator {
+
+    private static final int START_PRICE = 100;
+    private static final int DIVIDE_PRICE = 10;
+    private static final String ERROR_MESSAGE = "상품 가격은 %d원부터 시작하며, %d원으로 나누어떨어져야 합니다.";
+
+    public Map<Coin, Integer> generate(int price) {
+        validate(price);
+        Map<Coin, Integer> result = new HashMap<>();
+        List<Integer> cases = initCases();
+        while (price != 0) {
+            removeImpossibleCase(price, cases);
+            storeCoinBox(result, cases);
+        }
+        return result;
+    }
+
+    private void validate(int price) {
+        if (price < START_PRICE || price % DIVIDE_PRICE != 0) {
+            throw new IllegalArgumentException(
+                    String.format(ERROR_MESSAGE, START_PRICE, DIVIDE_PRICE));
+        }
+    }
+
+    private List<Integer> initCases() {
+        return Arrays.stream(Coin.values())
+                .mapToInt(Coin::getAmount)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    private static void removeImpossibleCase(int price, List<Integer> cases) {
+        while (cases.get(0) > price) {
+            cases.remove(0);
+        }
+    }
+
+    private void storeCoinBox(Map<Coin, Integer> result, List<Integer> cases) {
+        Coin coin = getCoin(cases);
+        result.put(coin, result.getOrDefault(coin, 0) + 1);
+    }
+
+    private Coin getCoin(List<Integer> cases) {
+        int amount = Randoms.pickNumberInList(cases);
+        return searchCoin(amount);
+    }
+
+    private Coin searchCoin(int amount) {
+        return Arrays.stream(Coin.values())
+                .filter(coin -> coin.getAmount() == amount)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException());
+    }
+}
