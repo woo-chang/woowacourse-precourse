6440f4ada4f0f276dad6784ee22cc3dd77f8dea2
woo-chang <clllickme@naver.com>
2022-12-11 14:02:02 +0900

    feat(OutputView): 메시지를 출력하는 기능 구현

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
@@ -4,6 +4,10 @@ public class OutputView {
 
     private final static String ERROR_PREFIX = "[ERROR] %s";
 
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
     public static void printErrorMessage(String message) {
         System.out.printf((ERROR_PREFIX) + "%n", message);
     }
