3d60910024a57d3f44a51f9ecb75cd90ffe04686
woo-chang <clllickme@naver.com>
2022-12-11 15:49:18 +0900

    chore(ProductInfo): 사용하지 않는 라이브러리 제거

--- java-vendingmachine-precourse-main/src/test/java/vendingmachine/model/ProductInfoTest.java
+++ java-vendingmachine-precourse-main/src/test/java/vendingmachine/model/ProductInfoTest.java
@@ -3,7 +3,6 @@ package vendingmachine.model;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
