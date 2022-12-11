6516ca08c97fea027f43739f7b0f20d0c4d850a3
woo-chang <clllickme@naver.com>
2022-12-11 17:20:53 +0900

    feat(ProductBox): 상품을 살 수 있는지 확인 후 상품을 구매하는 기능 구현

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/model/ProductBox.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/model/ProductBox.java
@@ -9,4 +9,35 @@ public class ProductBox {
     public ProductBox(Map<String, ProductInfo> productBox) {
         this.productBox = productBox;
     }
+
+    public boolean canBuy(int money) {
+        if (productBox.isEmpty()) {
+            return false;
+        }
+        if (money < searchCheapestPrice()) {
+            return false;
+        }
+        return true;
+    }
+
+    private int searchCheapestPrice() {
+        return productBox.values().stream()
+                .mapToInt(ProductInfo::getPrice)
+                .min()
+                .getAsInt();
+    }
+
+    public int buy(String name) {
+        if (!productBox.containsKey(name)) {
+            throw new IllegalArgumentException("존재하지 않는 상품입니다.");
+        }
+        ProductInfo productInfo = productBox.get(name);
+        ProductInfo update = productInfo.updateByPurchase();
+        if (update.getCount() == 0) {
+            productBox.remove(name);
+        } else {
+            productBox.put(name, update);
+        }
+        return update.getPrice();
+    }
 }
