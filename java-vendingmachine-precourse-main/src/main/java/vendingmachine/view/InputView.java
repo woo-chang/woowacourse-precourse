73f311bc2f053b2be5888a8bad933f9eede7e994
woo-chang <clllickme@naver.com>
2022-12-11 14:08:03 +0900

    feat(InputView): 입력 받기 전 입력 문구 출력하는 기능 구현

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/InputView.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/InputView.java
@@ -8,10 +8,15 @@ import vendingmachine.util.NumericConverter;
 
 public class InputView {
 
+    private static final String MONEY_ON_HAND_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private static final String PRODUCTS_MESSAGE = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String MONEY_ON_INPUT_MESSAGE = "투입 금액을 입력해 주세요.";
+    private static final String PRODUCT_NAME_MESSAGE = "구매할 상품명을 입력해 주세요.";
+
     public int readMoneyOnHand() {
         while (true) {
             try {
-                String input = Console.readLine();
+                String input = inputMoneyOnHand();
                 return NumericConverter.convert(input);
             } catch (IllegalArgumentException e) {
                 OutputView.printErrorMessage(e.getMessage());
@@ -19,7 +24,13 @@ public class InputView {
         }
     }
 
+    private String inputMoneyOnHand() {
+        OutputView.printMessage(MONEY_ON_HAND_MESSAGE);
+        return Console.readLine();
+    }
+
     public List<String> readProducts() {
+        OutputView.printMessage(PRODUCTS_MESSAGE);
         String input = Console.readLine();
         return Arrays.stream(input.split(";"))
                 .collect(Collectors.toList());
@@ -28,7 +39,7 @@ public class InputView {
     public int readMoneyOnInput() {
         while (true) {
             try {
-                String input = Console.readLine();
+                String input = inputMoneyOnInput();
                 return NumericConverter.convert(input);
             } catch (IllegalArgumentException e) {
                 OutputView.printErrorMessage(e.getMessage());
@@ -36,7 +47,13 @@ public class InputView {
         }
     }
 
+    private String inputMoneyOnInput() {
+        OutputView.printMessage(MONEY_ON_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
     public String readProductName() {
+        OutputView.printMessage(PRODUCT_NAME_MESSAGE);
         return Console.readLine();
     }
 }
