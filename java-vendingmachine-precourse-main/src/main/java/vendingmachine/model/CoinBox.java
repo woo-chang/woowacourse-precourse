489120c893975ae2201836e7cbb0eed440971b49
woo-chang <clllickme@naver.com>
2022-12-11 17:19:43 +0900

    feat(CoinBox): 남아있는 잔돈을 반환하는 기능 구현

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/model/CoinBox.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/model/CoinBox.java
@@ -1,5 +1,6 @@
 package vendingmachine.model;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CoinBox {
@@ -8,5 +9,17 @@ public class CoinBox {
 
     public CoinBox(Map<Coin, Integer> stringBox) {
         this.stringBox = stringBox;
+    }
+
+    public Map<Coin, Integer> getRemain(int money) {
+        Map<Coin, Integer> result = new LinkedHashMap<>();
+        for (Coin coin : Coin.values()) {
+            if (coin.getAmount() < money && stringBox.get(coin) > 0) {
+                int count = Math.min(stringBox.get(coin), money / coin.getAmount());
+                result.put(coin, count);
+                money -= coin.getAmount() * count;
+            }
+        }
+        return result;
     }
 }
