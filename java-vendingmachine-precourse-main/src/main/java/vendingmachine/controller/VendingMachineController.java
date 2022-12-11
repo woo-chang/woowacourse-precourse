ac51270d8c6f81417aa18650d1c5c057c6638249
woo-chang <clllickme@naver.com>
2022-12-11 17:16:02 +0900

    feat(VendingMachineController): 자판기를 동작시키는 기능 구현

---
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -0,0 +1,72 @@
+package vendingmachine.controller;
+
+import java.util.List;
+import java.util.Map;
+import vendingmachine.model.Coin;
+import vendingmachine.model.CoinBox;
+import vendingmachine.model.ProductBox;
+import vendingmachine.model.ProductInfo;
+import vendingmachine.model.VendingMachine;
+import vendingmachine.service.CoinBoxGenerator;
+import vendingmachine.service.ProductBoxGenerator;
+import vendingmachine.view.InputView;
+import vendingmachine.view.OutputView;
+
+public class VendingMachineController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final CoinBoxGenerator coinBoxGenerator = new CoinBoxGenerator();
+    private final ProductBoxGenerator productBoxGenerator = new ProductBoxGenerator();
+
+    public void run() {
+        CoinBox coinBox = generateCoinBox();
+        ProductBox productBox = generateProductBox();
+        int inputMoney = generateOnInput();
+        VendingMachine vendingMachine = new VendingMachine(coinBox, productBox, inputMoney);
+        outputView.printInputAmount(vendingMachine.getInputMoney());
+        useUtilEnd(vendingMachine);
+        outputView.printRemainCoins(vendingMachine.getRemain());
+    }
+
+    private CoinBox generateCoinBox() {
+        while (true) {
+            try {
+                int money = inputView.readMoneyOnHand();
+                Map<Coin, Integer> coinBox = coinBoxGenerator.generate(money);
+                outputView.printHoldingCoins(coinBox);
+                return new CoinBox(coinBox);
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private ProductBox generateProductBox() {
+        while (true) {
+            try {
+                List<String> products = inputView.readProducts();
+                Map<String, ProductInfo> productBox = productBoxGenerator.generate(products);
+                return new ProductBox(productBox);
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private int generateOnInput() {
+        return inputView.readMoneyOnInput();
+    }
+
+    private void useUtilEnd(VendingMachine vendingMachine) {
+        while (vendingMachine.isUsable()) {
+            try {
+                String name = inputView.readProductName();
+                vendingMachine.use(name);
+                outputView.printInputAmount(vendingMachine.getInputMoney());
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+}
