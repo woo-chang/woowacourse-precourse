bee25fb81bff719317d0eca54af46cf656cd5610
woo-chang <clllickme@naver.com>
2022-12-11 16:09:15 +0900

    feat(OutputView): 자판기가 보유한 동전을 출력하는 기능 구현

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
@@ -1,8 +1,19 @@
 package vendingmachine.view;
 
+import java.util.Arrays;
+import java.util.Map;
+import vendingmachine.model.Coin;
+
 public class OutputView {
 
     private final static String ERROR_PREFIX = "[ERROR] %s";
+
+    public void printHoldingCoins(Map<Coin, Integer> coins) {
+        System.out.println("자판기가 보유한 동전");
+        Arrays.stream(Coin.values())
+                .forEach(coin -> System.out.println(
+                        String.format("%d원 - %d개", coin.getAmount(), coins.get(coin))));
+    }
 
     public static void printMessage(String message) {
         System.out.println(message);
