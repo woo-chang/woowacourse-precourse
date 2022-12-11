75ed79ec5f673160f1e2a034ee7810335b846486
woo-chang <clllickme@naver.com>
2022-12-11 15:49:40 +0900

    test(ProductBoxGenerator): 입력 정보를 바탕으로 상품 박스를 생성하는 기능 테스트

---
+++ java-vendingmachine-precourse-main/src/test/java/vendingmachine/service/ProductBoxGeneratorTest.java
@@ -0,0 +1,50 @@
+package vendingmachine.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import vendingmachine.model.ProductInfo;
+
+class ProductBoxGeneratorTest {
+
+    private final ProductBoxGenerator productBoxGenerator = new ProductBoxGenerator();
+
+    @DisplayName("상품 정보를 올바르게 저장한다.")
+    @Test
+    void storeProduct() {
+        List<String> infos = new ArrayList<>(
+                List.of("[콜라,1500,20]", "[사이다,1000,10]")
+        );
+
+        Map<String, ProductInfo> productBox = productBoxGenerator.generate(infos);
+
+        assertThat(productBox.values().size()).isEqualTo(2);
+    }
+
+    @DisplayName("상품 이름에 공백으로만 이루어진 경우 예외가 발생한다.")
+    @Test
+    void invalidProductName() {
+        List<String> infos = new ArrayList<>(
+                List.of("[ ,1500,20]", "[   ,1000,10]")
+        );
+
+        assertThatThrownBy(() -> productBoxGenerator.generate(infos))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("중복되는 상품을 입력하면 예외가 발생한다.")
+    @Test
+    void duplicateProduct() {
+        List<String> infos = new ArrayList<>(
+                List.of("[콜라,1500,20]", "[콜라,1000,10]")
+        );
+
+        assertThatThrownBy(() -> productBoxGenerator.generate(infos))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
