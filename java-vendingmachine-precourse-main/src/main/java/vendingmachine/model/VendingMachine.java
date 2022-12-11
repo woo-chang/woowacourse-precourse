fac9aee800fddfbae148ee98f75bc2545cbeabc2
woo-chang <clllickme@naver.com>
2022-12-11 17:22:35 +0900

    feat(VendingMachine): 자판기가 사용가능한 상태 확인 후 자판기를 사용하고 잔돈을 반환하는 기능 구현

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/model/VendingMachine.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/model/VendingMachine.java
@@ -1,12 +1,33 @@
 package vendingmachine.model;
 
+import java.util.Map;
+
 public class VendingMachine {
 
     private final CoinBox coinBox;
     private final ProductBox productBox;
+    private int inputMoney;
 
-    public VendingMachine(CoinBox coinBox, ProductBox productBox) {
+    public VendingMachine(CoinBox coinBox, ProductBox productBox, int inputMoney) {
         this.coinBox = coinBox;
         this.productBox = productBox;
+        this.inputMoney = inputMoney;
+    }
+
+    public boolean isUsable() {
+        return productBox.canBuy(inputMoney);
+    }
+
+    public void use(String name) {
+        int price = productBox.buy(name);
+        inputMoney -= price;
+    }
+
+    public Map<Coin, Integer> getRemain() {
+        return coinBox.getRemain(inputMoney);
+    }
+
+    public int getInputMoney() {
+        return inputMoney;
     }
 }
