1327e7d9b0d30b551bf0536fc7e954839a81c5b8
woo-chang <clllickme@naver.com>
2022-12-12 10:32:00 +0900

    refactor(OutputView): 의미있는 메서드 이름으로 수정

--- java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
+++ java-vendingmachine-precourse-main/src/main/java/vendingmachine/view/OutputView.java
@@ -7,24 +7,28 @@ import vendingmachine.model.Coin;
 public class OutputView {
 
     private final static String ERROR_PREFIX = "[ERROR] %s";
+    private final static String COIN_ON_HAND_MESSAGE = "자판기가 보유한 동전";
+    private final static String COIN_FORMAT = "%d원 - %d개";
+    private final static String MONEY_OF_INPUT_MESSAGE = "투입 금액: %d원";
+    private final static String REMAINED_MESSAGE = "잔돈";
 
-    public void printHoldingCoins(Map<Coin, Integer> coins) {
-        System.out.println("자판기가 보유한 동전");
+    public void printCoinOnHand(Map<Coin, Integer> coins) {
+        System.out.println(COIN_ON_HAND_MESSAGE);
         Arrays.stream(Coin.values())
                 .forEach(coin -> System.out.println(
-                        String.format("%d원 - %d개", coin.getAmount(), coins.get(coin))));
+                        String.format(COIN_FORMAT, coin.getAmount(), coins.get(coin))));
     }
 
-    public void printInputAmount(int money) {
-        System.out.println(String.format("투입 금액: %d원", money));
+    public void printMoneyOfInput(int money) {
+        System.out.println(String.format(MONEY_OF_INPUT_MESSAGE, money));
     }
 
-    public void printRemainCoins(Map<Coin, Integer> coins) {
-        System.out.println("잔돈");
+    public void printRemainedCoins(Map<Coin, Integer> coins) {
+        System.out.println(REMAINED_MESSAGE);
         for (Coin coin : Coin.values()) {
             if (coins.containsKey(coin)) {
                 System.out.println(
-                        String.format("%d원 - %d개", coin.getAmount(), coins.get(coin)));
+                        String.format(COIN_FORMAT, coin.getAmount(), coins.get(coin)));
             }
         }
     }
