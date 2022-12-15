2586dd90f9d1dee99335aec9e4d8303d25b91143
woo-chang <clllickme@naver.com>
2022-12-15 22:48:45 +0900

    feat(OutputView): 에러 메시지를 출력하는 기능을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/view/OutputView.java
+++ java-subway-path-precourse/src/main/java/subway/view/OutputView.java
@@ -2,7 +2,13 @@ package subway.view;
 
 public class OutputView {
 
+    private static final String ERROR = "[ERROR] %s";
+
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(String.format(ERROR, message));
     }
 }
