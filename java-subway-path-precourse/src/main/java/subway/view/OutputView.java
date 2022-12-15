c220d24039e6408dc98bf18bd274986ebaa7f711
woo-chang <clllickme@naver.com>
2022-12-15 23:44:09 +0900

    feat(OutputView): 메인 화면 정보를 출력하는 기능을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/view/OutputView.java
+++ java-subway-path-precourse/src/main/java/subway/view/OutputView.java
@@ -1,8 +1,18 @@
 package subway.view;
 
+import subway.domain.MainMenu;
+
 public class OutputView {
 
     private static final String ERROR = "[ERROR] %s";
+    private static final String MAIN_MENU = "## 메인 화면";
+
+    public void printMainMenu() {
+        System.out.println(MAIN_MENU);
+        for (MainMenu menu : MainMenu.values()) {
+            System.out.println(menu);
+        }
+    }
 
     public static void printMessage(String message) {
         System.out.println(message);
