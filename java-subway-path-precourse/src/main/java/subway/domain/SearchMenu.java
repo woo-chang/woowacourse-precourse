293803f5934835c944d71699da44750fed24194a
woo-chang <clllickme@naver.com>
2022-12-15 23:45:42 +0900

    feat(SearchMenu): 값에 알맞은 이름을 설정하고 출력을 위한 toString을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/domain/SearchMenu.java
+++ java-subway-path-precourse/src/main/java/subway/domain/SearchMenu.java
@@ -4,15 +4,17 @@ import java.util.Arrays;
 
 public enum SearchMenu {
 
-    DISTANCE("1"),
-    TIME("2"),
-    BACK("B");
+    DISTANCE("1", "최단 거리"),
+    TIME("2", "최소 시간"),
+    BACK("B", "돌아가기");
 
-    SearchMenu(String value) {
+    SearchMenu(String value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     private final String value;
+    private final String name;
 
     public String getValue() {
         return value;
@@ -23,5 +25,10 @@ public enum SearchMenu {
                 .filter(menu -> menu.getValue().equals(value))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 조회 메뉴입니다."));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s. %s", value, name);
     }
 }
