dafd901fdd22e587b666b42381c5ad5a6dbbf3e7
woo-chang <clllickme@naver.com>
2022-12-15 23:34:19 +0900

    feat(InputView): 도착역을 입력하는 기능을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/view/InputView.java
+++ java-subway-path-precourse/src/main/java/subway/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
 
     private static final String MENU_MESSAGE = "## 원하는 기능을 선택하세요.";
     private static final String START_STATION = "## 출발역을 입력하세요.";
+    private static final String ARRIVAL_STATION = "## 도착역을 입력하세요.";
 
     public MainMenu readMainMenu(Scanner scanner) {
         while (true) {
@@ -37,6 +38,17 @@ public class InputView {
         while (true) {
             try {
                 OutputView.printMessage(START_STATION);
+                return StationRepository.findByName(scanner.nextLine());
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    public Station readArrivalStation(Scanner scanner) {
+        while (true) {
+            try {
+                OutputView.printMessage(ARRIVAL_STATION);
                 return StationRepository.findByName(scanner.nextLine());
             } catch (IllegalArgumentException e) {
                 OutputView.printErrorMessage(e.getMessage());
