e7532bdd3ef30366016499979d3fbe0c52976fe8
woo-chang <clllickme@naver.com>
2022-12-15 23:02:31 +0900

    feat(InputView): 메인 메뉴를 입력하는 기능을 구현한다.

---
+++ java-subway-path-precourse/src/main/java/subway/view/InputView.java
@@ -0,0 +1,21 @@
+package subway.view;
+
+import java.util.Scanner;
+import subway.domain.MainMenu;
+
+public class InputView {
+
+    private static final String MENU_MESSAGE = "## 원하는 기능을 선택하세요.";
+
+    public MainMenu readMainMenu(Scanner scanner) {
+        while (true) {
+            try {
+                OutputView.printMessage(MENU_MESSAGE);
+                return MainMenu.from(scanner.nextLine());
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+}
