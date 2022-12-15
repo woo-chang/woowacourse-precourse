a020428bc46fa6609e4b4281757cd52e6375bc6e
woo-chang <clllickme@naver.com>
2022-12-15 22:54:13 +0900

    feat(MainMenu): 메인 메뉴 선택 옵션 및 메인 메뉴의 값과 일치하는 메인 메뉴를 반환하는 기능을 구현한다.

---
+++ java-subway-path-precourse/src/main/java/subway/domain/MainMenu.java
@@ -0,0 +1,27 @@
+package subway.domain;
+
+import java.util.Arrays;
+
+public enum MainMenu {
+
+    ROUTE("1"),
+    QUIT("Q");
+
+    MainMenu(String value) {
+        this.value = value;
+    }
+
+    private final String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public static MainMenu from(String value) {
+        Arrays.stream(MainMenu.values())
+                .filter(menu -> menu.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메인 메뉴입니다."));
+    }
+
+}
