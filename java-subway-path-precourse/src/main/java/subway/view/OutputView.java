73e628e2a3f6ebd1c424301751b653ff3fdbd138
woo-chang <clllickme@naver.com>
2022-12-15 23:47:07 +0900

    feat(OutputView): 경로 기준을 출력하는 기능을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/view/OutputView.java
+++ java-subway-path-precourse/src/main/java/subway/view/OutputView.java
@@ -1,15 +1,24 @@
 package subway.view;
 
 import subway.domain.MainMenu;
+import subway.domain.SearchMenu;
 
 public class OutputView {
 
     private static final String ERROR = "[ERROR] %s";
     private static final String MAIN_MENU = "## 메인 화면";
+    private static final String SEARCH_MENU = "## 경로 기준";
 
     public void printMainMenu() {
         System.out.println(MAIN_MENU);
         for (MainMenu menu : MainMenu.values()) {
+            System.out.println(menu);
+        }
+    }
+
+    public void printSearchMenu() {
+        System.out.println(SEARCH_MENU);
+        for (SearchMenu menu : SearchMenu.values()) {
             System.out.println(menu);
         }
     }
