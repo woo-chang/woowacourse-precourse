4419edec518e29a49ed4211b3d9b8196ed589d68
woo-chang <clllickme@naver.com>
2022-12-15 22:31:44 +0900

    feat(SubwayConfig): 지하철 노선을 등록하는 기능을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/util/SubwayConfig.java
+++ java-subway-path-precourse/src/main/java/subway/util/SubwayConfig.java
@@ -3,7 +3,9 @@ package subway.util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import subway.domain.Line;
 import subway.domain.Station;
+import subway.repository.LineRepository;
 import subway.repository.StationRepository;
 
 public class SubwayConfig {
@@ -13,6 +15,7 @@ public class SubwayConfig {
 
     public static void configure() {
         stationConfigure();
+        lineConfigure();
     }
 
     private static void stationConfigure() {
@@ -23,6 +26,15 @@ public class SubwayConfig {
                 .map(Station::new)
                 .collect(Collectors.toList());
         stations.forEach(StationRepository::addStation);
+    }
+
+    private static void lineConfigure() {
+        LineRepository.deleteAll();
+        List<String> lineNames = new ArrayList<>(List.of("2호선", "3호선", "신분당선"));
+        List<Line> lines = lineNames.stream()
+                .map(Line::new)
+                .collect(Collectors.toList());
+        lines.forEach(LineRepository.addLine());
     }
 
 }
