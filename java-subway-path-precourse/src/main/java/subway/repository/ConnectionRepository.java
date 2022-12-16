90350f27957e7a76a45ad51be6ae90d66744db9c
woo-chang <clllickme@naver.com>
2022-12-16 10:40:26 +0900

    feat(ConnectionRepository): 역으로 연결 정보를 찾는 기능을 구현한다.

--- java-subway-path-precourse/src/main/java/subway/repository/ConnectionRepository.java
+++ java-subway-path-precourse/src/main/java/subway/repository/ConnectionRepository.java
@@ -1,6 +1,7 @@
 package subway.repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,8 +12,16 @@ public class ConnectionRepository {
 
     private static final Map<Station, List<Connection>> connectionDB = new HashMap<>();
 
+    public static Map<Station, List<Connection>> connections() {
+        return Collections.unmodifiableMap(connectionDB);
+    }
+
     public static void addConnection(Station station, Connection connection) {
         connectionDB.computeIfAbsent(station, c -> new ArrayList<>()).add(connection);
+    }
+
+    public static List<Connection> findByStation(Station station) {
+        return connectionDB.get(station);
     }
 
     public static void deleteAll() {
