dfaaaa945281854200e49ebe2eb20b4c0adaba83
woo-chang <clllickme@naver.com>
2022-12-16 15:16:49 +0900

    feat(MatchingStatus): 매칭 여부 설명으로 해당하는 매칭 여부를 반환하는 기능 구현

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/MatchingStatus.java
@@ -0,0 +1,22 @@
+package pairmatching.domain;
+
+import java.util.Arrays;
+
+public enum MatchingStatus {
+
+    YES("네"),
+    NO("아니오");
+
+    MatchingStatus(String value) {
+        this.value = value;
+    }
+
+    private final String value;
+
+    public static MatchingStatus from(String value) {
+        return Arrays.stream(MatchingStatus.values())
+                .filter(matchingStatus -> matchingStatus.value.equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 매칭 여부입니다."));
+    }
+}
