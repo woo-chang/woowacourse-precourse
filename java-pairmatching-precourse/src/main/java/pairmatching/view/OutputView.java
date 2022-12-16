ffc53d2fccbe6ee64c5f163b5c9d29ddf2fb123d
woo-chang <clllickme@naver.com>
2022-12-16 14:50:31 +0900

    feat(OutputView): 에러 메시지 출력하는 기능 구현

--- java-pairmatching-precourse/src/main/java/pairmatching/view/OutputView.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/view/OutputView.java
@@ -2,7 +2,13 @@ package pairmatching.view;
 
 public class OutputView {
 
+    private static final String ERROR = "[ERROR] %s";
+
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println(String.format(ERROR, message));
     }
 }
