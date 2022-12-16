35abbdde0821eee25f676e25b82d3bf3108e33c4
woo-chang <clllickme@naver.com>
2022-12-16 10:42:13 +0900

    chore: 컨벤션 수정, 디렉토리 위치 수정

---
+++ java-subway-path-precourse/src/main/java/subway/repository/LineRepository.java
@@ -0,0 +1,28 @@
+package subway.repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import subway.domain.Line;
+
+public class LineRepository {
+
+    private static final List<Line> lines = new ArrayList<>();
+
+    public static List<Line> lines() {
+        return Collections.unmodifiableList(lines);
+    }
+
+    public static void addLine(Line line) {
+        lines.add(line);
+    }
+
+    public static boolean deleteLineByName(String name) {
+        return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static void deleteAll() {
+        lines.clear();
+    }
+}
