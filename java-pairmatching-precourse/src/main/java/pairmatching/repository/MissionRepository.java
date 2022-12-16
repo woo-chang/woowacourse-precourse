452fce0f32722902b766cf723217ec76853f8ff6
woo-chang <clllickme@naver.com>
2022-12-16 14:47:50 +0900

    refactor(MissionRepository): 미션 저장소 생성 시 초기화하도록 수정

--- java-pairmatching-precourse/src/main/java/pairmatching/repository/MissionRepository.java
+++ java-pairmatching-precourse/src/main/java/pairmatching/repository/MissionRepository.java
@@ -1,5 +1,6 @@
 package pairmatching.repository;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,10 +9,18 @@ import pairmatching.domain.Level;
 
 public class MissionRepository {
 
-    private static final Map<Level, List<String>> missions = new HashMap<>();
+    private static final Map<Level, List<String>> missions = new HashMap<>(generateMissions());
 
-    public static void addMission(Level level, String mission) {
-        missions.computeIfAbsent(level, m -> new ArrayList<>()).add(mission);
+    private static Map<Level, ArrayList<String>> generateMissions() {
+        return Map.ofEntries(
+                new SimpleEntry<>(Level.LEVEL1,
+                        new ArrayList<>(List.of("자동차경주", "로또", "숫자야구게임"))),
+                new SimpleEntry<>(Level.LEVEL2,
+                        new ArrayList<>(List.of("장바구니", "결제", "지하철노선도"))),
+                new SimpleEntry<>(Level.LEVEL3, new ArrayList<>()),
+                new SimpleEntry<>(Level.LEVEL4, new ArrayList<>(List.of("성능개선", "배포"))),
+                new SimpleEntry<>(Level.LEVEL5, new ArrayList<>())
+        );
     }
 
     public static List<String> findByLevel(Level level) {
