82af43edeb526d014e192c21ced56725687a6155
woo-chang <clllickme@naver.com>
2022-12-16 14:32:01 +0900

    feat(Crew): 과정과 이름 정보를 가지는 크루 구현

---
+++ java-pairmatching-precourse/src/main/java/pairmatching/domain/Crew.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+public class Crew {
+
+    private final Course course;
+    private final String name;
+
+    public Crew(Course course, String name) {
+        this.course = course;
+        this.name = name;
+    }
+}
